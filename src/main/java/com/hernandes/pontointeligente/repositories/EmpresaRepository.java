67d1cf2115d7c2a420614af6f1ab3b5601aeb582
Hernandes Santos <hernandes.santos1@gmail.com>
2018-06-18 21:14:52 -0300

    repositorios e utils

---
+++ src/main/java/com/hernandes/pontointeligente/repositories/EmpresaRepository.java
@@ -0,0 +1,14 @@
+package com.hernandes.pontointeligente.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.hernandes.pontointeligente.entities.Empresa;
+
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
